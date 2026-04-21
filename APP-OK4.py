d9ea64e76502a129354b8f49a888121354dc154c
wu3052 <wu3052@gmail.com>
2026-04-21 23:23:20 +0800

    Update APP-OK4.py

--- APP-OK4.py
+++ APP-OK4.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+from scipy.signal import argrelextrema
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
